00657939ca0edf3de5b066c8b97eeb019e778af3
IGOR GUTIERREZ <ac.igor.gutierrez.dc@gmail.com>
2024-02-17 15:42:33 -0300

    Updates.

--- ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f8147ab2c48c7761c76fd7c593b000dd7973fcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c3f0a753659ffeb4bbd42b274aad2da0be6ffde")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
